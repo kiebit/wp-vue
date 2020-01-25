6454eb9a3823ec45381bec083cfe2d3509e5e9d7
Ahtsham Farooq <kahtshamfarooq@gmail.com>
2020-01-25 19:26:19 +0500

    Uploading main.js

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// src/main.js
+import Vue from 'vue';
+
+import './../node_modules/bulma/css/bulma.css';
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
